d9a0eaf16f00cb0a08c163c69c659236f0bc9cde
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-05-13 10:10:29 +0530

    added neon dialect

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,14 +1,12 @@
-import { Kysely, PostgresDialect } from "kysely";
-import { Pool } from "pg";
+import { Kysely } from "kysely";
+import { NeonDialect } from "kysely-neon";
 
 import { env } from "~/env.js";
 
 import type { DB } from "./types";
 
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    connectionString: env.DATABASE_URL,
-  }),
+const dialect = new NeonDialect({
+  connectionString: env.DATABASE_URL,
 });
 
 // Database interface is passed to Kysely's constructor, and from now on, Kysely
